1d45bda7e9c62241426560e54ea92d249b6397e8
Junior <juniorr452@gmail.com>
2018-07-28 09:53:55 -0300

    UI Carro
    Criei uma UI para mostrar informações dos sensores dos carros, velocidade e, futuramente, o fitness

---
+++ Assets/Scripts/UICarro.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.Vehicles.Car;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CarController))]
+[RequireComponent(typeof(SensoresCarro))]
+
+public class UICarro : MonoBehaviour 
+{ 
+	public Text infoCarroText;
+	public Text sensoresCarroText;
+	
+	private CarController carController;
+	private SensoresCarro sensoresCarro;
+
+	// Use this for initialization
+	void Start () 
+	{
+		carController = GetComponent<CarController>();
+		sensoresCarro = GetComponent<SensoresCarro>();
+	}	
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		StringBuilder sbSensoresCarro = new StringBuilder();
+		StringBuilder sbInfoCarro     = new StringBuilder();
+
+		// Texto do sensores carro
+		for(int i = 0; i < sensoresCarro.distanciaSensores.Length; i++)
+		{
+			float distancia = sensoresCarro.distanciaSensores[i];
+
+			sbSensoresCarro.Append(" | ");
+			sbSensoresCarro.Append(distancia.ToString("0.0"));
+		}
+
+		sbSensoresCarro.Append(" |");
+
+		// Texto do info carro (Velocidade, distância percorrida...)
+		sbInfoCarro.Append(carController.CurrentSpeed.ToString("0.0"));
+		sbInfoCarro.AppendLine(" mph");
+
+		sbInfoCarro.Append("Fit: ");
+		// TODO: Colocar a distância percorrida ou algo do tipo aqui
+		sbInfoCarro.Append(0);
+
+		// Atualizar a UI
+		sensoresCarroText.text = sbSensoresCarro.ToString();
+		infoCarroText.text     = sbInfoCarro.ToString();
+	}
+}
